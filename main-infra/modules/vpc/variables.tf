637990e017c10948ddcfec6d66873b75bcee94cd
Yash Raut <136910892+yash2815@users.noreply.github.com>
2025-08-01 13:20:24 +0530

    Update variables.tf

--- main-infra/modules/vpc/variables.tf
+++ main-infra/modules/vpc/variables.tf
@@ -21,6 +21,7 @@ variable "private_subnet_cidrs" {
 variable "availability_zones" {
   description = "List of availability zones"
   type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
 }
 
 variable "enable_nat_gateway" {
