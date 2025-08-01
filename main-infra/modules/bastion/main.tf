59164ab04102351688510114fffa203236b608f0
Yash Raut <136910892+yash2815@users.noreply.github.com>
2025-08-01 12:11:31 +0530

    Update main.tf

--- main-infra/modules/bastion/main.tf
+++ main-infra/modules/bastion/main.tf
@@ -42,7 +42,7 @@ resource "aws_security_group" "bastion_sg" {
 resource "aws_instance" "bastion" {
   ami                         = var.ami_id
   instance_type               = var.instance_type
-  subnet_id                   = var.subnet_id
+  subnet_id                   = aws_subnet.public[1].id
   vpc_security_group_ids      = [aws_security_group.bastion_sg.id]
   associate_public_ip_address = true
   key_name                    = aws_key_pair.bastion_key.key_name
